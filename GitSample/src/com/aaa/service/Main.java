b5c93c750b425ea4691d866e12429616e3307e0a
Thandav <thandav007@gmail.com>
2015-10-18 11:29:35 -0400

    First Commit

---
+++ GitSample/src/com/aaa/service/Main.java
@@ -0,0 +1,19 @@
+package com.aaa.service;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Data data = new Data();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	public static void displayGreeting(){
+		
+		System.out.println("HELLO DER");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
